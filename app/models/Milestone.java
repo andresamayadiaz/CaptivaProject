b72db4aa1ef45faf4dc3e5ce7cd1d9268e7bbdc1
Andres Amaya Diaz <andres.amaya.diaz@gmail.com>
2012-01-27 20:49:42 -0600

    local

---
+++ app/models/Milestone.java
@@ -0,0 +1,31 @@
+package models;
+
+import play.*;
+import play.data.validation.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+
+import java.util.*;
+
+@Entity
+public class Milestone extends Model {
+    
+	@Required(message = "Name is requiered")
+	@MaxSize(50)
+	public String Name;
+	
+	@Required(message = "Description is requiered")
+	public String Description;
+	
+	public Date DueDate;
+	
+	@Required(message = "Owner is required")
+	@ManyToOne
+	public User Owner;
+	
+	public String toStrign(){
+		return Name;
+	}
+	
+}
